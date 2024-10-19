0b574f5a729e72c60c7d7a9a1a766c1384332645
Teamishant65 <fastwin.hack.8879@gmail.com>
2024-10-19 13:59:07 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+// Change this to your connection info.
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'one_device';
+$DATABASE_PASS = '123456';
+$DATABASE_NAME = 'one_device';
+// Try and connect using the info above.
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+$mysql = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+$conn = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+?>
+
+
